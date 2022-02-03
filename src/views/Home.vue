4d50d4151a6b79a97f3c10d66db70d32b63e8f72
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-02-02 23:17:55 -0500

    fix rotation speed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center gap-4 justify-center w-full h-full">
     <img
       src="/ariimaw_black.svg"
-      class="text-white w-48 animate-spin animate-duration-6000"
+      class="text-white w-48 animate-spin animate-duration-60000"
       alt="Ariimaw"
     />
 

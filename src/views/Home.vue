af99792a2668bc059d1c2ca3ca28da67fd283d1c
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-02-02 22:22:11 -0500

    fix broken rollup import

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center w-full h-full">
     <img
-      src="ariimaw_black.svg"
+      src="/ariimaw_black.svg"
       class="text-white w-48 animate-spin animate-duration-60000"
       alt="Ariimaw"
     />
